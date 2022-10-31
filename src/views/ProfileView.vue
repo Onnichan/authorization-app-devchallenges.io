64bd08f480ab7bcff83a1301c2e1116ce67d1534
Onnichan <dyangeltk17@gmail.com>
2022-10-31 08:41:52 -0500

    :sparkles: Add http request for oauth login success

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,19 @@
 <script setup>
+import { onMounted, ref } from "vue";
 import TemplateProfileForm from "../components/template/TemplateProfileForm.vue";
+
+const user = ref(null);
+
+onMounted(() => {
+  console.log("mounted");
+  fetch("http://localhost:4000/api/v1/auth/login/success")
+    .then((success) => success.json())
+    .then((resp) => (user.value = resp));
+  console.log("user");
+  // user.value = login.json();
+});
 </script>
 <template>
+  {{user}}
   <TemplateProfileForm></TemplateProfileForm>
 </template>
