96f72004fc1d9f37a6c23770daffcccee2a574f7
Onnichan <dyangeltk17@gmail.com>
2022-10-31 08:44:53 -0500

    :sparkles: Add http request for oauth login success

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -6,9 +6,10 @@ const user = ref(null);
 
 onMounted(() => {
   // console.log("mounted");
-  fetch("http://localhost:4000/api/v1/auth/login/success")
-    .then((success) => success.json())
-    // .then((resp) => (user.value = resp));
+  fetch("http://localhost:4000/api/v1/auth/login/success").then((success) =>
+    success.json()
+  );
+  // .then((resp) => (user.value = resp));
   // console.log("user");
   // user.value = login.json();
 });
