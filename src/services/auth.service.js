e05f07a15c8c64ba9067e7950fc097c7c727d33d
Onnichan <dyangeltk17@gmail.com>
2022-11-04 08:35:43 -0500

    :sparkles: testing

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -8,7 +8,7 @@ const login = (user, pass) =>
 
 const oauthSuccessLogin = () => {
   console.log("here succes", import.meta.env.VITE_URL_BASE_API);
-  return ApiRequest.get("auth/login/success", { withCredentials: true });
+  return ApiRequest.get("auth/login/success");
 };
 
 const register = (name, lastname, email, password, confirmPassword) =>
